a2764bff0eb7c0eb7fad5b0d8b139a1fe41ec59f
chen <c19901008@qq.com>
2020-09-28 15:55:39 +0800

    myAlgorithm 2020/9/28提交第二题

--- myAlgorithm/main.cpp
+++ myAlgorithm/main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
-#include <map>
 using namespace std;
 
+int toutouzi(int x){
+    if(x<0)return 0;
+    else if(x==0)return 1;
+    return toutouzi(x-1)+toutouzi(x-2)+toutouzi(x-3)+toutouzi(x-4)+toutouzi(x-5)+toutouzi(x-6);
+}
 int main() {
     int n;
-    map<int,int> m;
     cin>>n;
-    for(int i=0;i<n;i++){
-        int x;
-        cin>>x;
-        m[x]++;
-        cout<<m[x];
-        if(i<n-1)cout<<' ';
-        else cout<<endl;
-    }
+    cout<<toutouzi(n)<<endl;
     return 0;
 }
