984811a56f62423fa9c31ae06bab665f0a791f1c
chen <c19901008@qq.com>
2020-09-29 09:41:01 +0800

    myAlgorithm 2020/9/28提交第四题

--- myAlgorithm/main.cpp
+++ myAlgorithm/main.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int MergeFind(int a[],int low,int mid,int high){
-    int i=low,j=mid+1;
-    if(a[i]==a[j])return a[j];
+int Merge(int a[],int low,int mid,int high){
+    int *tmpa;
+    int pairs=0;
+    int i=low,j=mid+1,k=0;
+    tmpa=(int *)malloc((high-low+1)*sizeof(int));
+    while(i<=mid&&j<=high){
+        if(a[i]<=a[j]){
+            tmpa[k]=a[i];
+            i++;k++;
+        }else{
+            tmpa[k]=a[j];
+            j++;k++;
+            pairs+=mid-i+1;
+        }
+    }
+    while(i<=mid){
+        tmpa[k]=a[i];
+        i++;k++;
+    }
+    while(j<=high){
+        tmpa[k]=a[j];
+        j++;k++;
+    }
+    for(k=0,i=low;i<high;k++,i++){
+        a[i]=tmpa[k];
+    }
+    free(tmpa);
+    return pairs;
 }
-int MergeZhongshu(int a[],int low,int high){
+int MergeSort(int a[],int low,int high){
     int mid;
     if(low<high){
         mid=(low+high)/2;
-        MergeZhongshu(a,low,mid);
-        MergeZhongshu(a,mid+1,high);
-        return MergeFind(a,low,mid,high);
+        MergeSort(a,low,mid);
+        int hcnt=MergeSort(a,mid+1,high);
+        Merge(a,low,mid,high);
+        return hcnt;
     }
 }
+
 int main() {
-    int a[]={1,2,2,3,3,3,5};
-    cout<<MergeZhongshu(a,0,6)<<endl;
+    int a[]={5,1,4,2,9};
+    cout<<MergeSort(a,0,4)<<endl;
     return 0;
 }
