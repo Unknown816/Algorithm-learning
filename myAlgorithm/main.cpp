99f8ca898d8a24aff8b26cbe67f8c9a9dd6f5a36
chen <c19901008@qq.com>
2020-10-26 15:23:24 +0800

    第五次提交 算法设计与分析课程

--- myAlgorithm/main.cpp
+++ myAlgorithm/main.cpp
@@ -1,67 +1,62 @@
 #include <iostream>
+#include <cstdio>
+#include <queue>
+#include <vector>
 using namespace std;
-void Merge(int a[],int low,int mid,int high){
-    int *tmpa;
-    int i=low,j=mid+1,k=0;
-    tmpa=(int *)malloc((high-low+1)*sizeof(int));
-    while(i<=mid&&j<=high){
-        if(a[i]<=a[j]){
-            tmpa[k]=a[i];
-            i++;k++;
-        }else{
-            tmpa[k]=a[j];
-            j++;k++;
+#define MaxN 10
+int n,m;
+const int dx[4]={1,-1,0,0};
+const int dy[4]={0,0,1,-1};
+void solve(vector<vector<char>>& map){
+    if(n==0)return;
+    queue<pair<int,int>>que;
+    for(int i=0;i<n;i++){
+        if(map[i][0]=='O')que.emplace(i,0);
+        if(map[i][m-1]=='O')que.emplace(i,m-1);
+    }
+    for(int i=0;i<m;i++){
+        if(map[0][i]=='O')que.emplace(0,i);
+        if(map[n-1][i]=='O')que.emplace(n-1,i);
+    }
+    while(!que.empty()){
+        int x=que.front().first,y=que.front().second;
+        que.pop();
+        map[x][y]='A';
+        for(int i=0;i<4;i++){
+            int mx=x+dx[i],my=y+dy[i];
+            if(mx<0||mx>=n||my<0||my>=m||map[mx][my]!='O')continue;
+            que.emplace(mx,my);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(map[i][j]=='A')map[i][j]='O';
+                else if(map[i][j]=='O')map[i][j]='X';
+            }
         }
     }
-    while(i<=mid){
-        tmpa[k]=a[i];
-        i++;k++;
-    }
-    while(j<=high){
-        tmpa[k]=a[j];
-        j++;k++;
-    }
-    for(k=0,i=low;i<=high;k++,i++){
-        a[i]=tmpa[k];
-    }
-    free(tmpa);
-}
-void MergeSort(int a[],int low,int high){
-    int mid;
-    if(low<high){
-        mid=(low+high)/2;
-        MergeSort(a,low,mid);
-        MergeSort(a,mid+1,high);
-        Merge(a,low,mid,high);
-    }
-}
-
-int BinSearch(int a[],int low,int high,int k){
-    int mid;
-    if(low<=high){
-        mid=(low+high)/2;
-        if(a[mid]==k){
-            return mid;
-        }
-        if(a[mid]>k){
-            return BinSearch(a,low,mid-1,k);
-        }else{
-            if(mid==low&&abs(high-low)==1)return -1;
-            if(a[high]==k)return high;
-            else return BinSearch(a,mid,high,k);
-        }
-    }else return -1;
 }
 int main() {
-    int n,c,a[100005],cnt=0;
-    cin>>n>>c;
+    n=4;
+    m=4;
+    //cin>>n>>m;
+    char a[MaxN][MaxN]={
+            {'X','X','X','X'},
+            {'X','O','O','X'},
+            {'X','X','O','X'},
+            {'X','O','X','X'}
+    };
+    vector<vector<char>> map(MaxN,vector<char>(MaxN));
     for(int i=0;i<n;i++){
-        cin>>a[i];
+        for(int j=0;j<m;j++){
+            //cin>>a[i][j];
+            map[i][j]=a[i][j];
+        }
     }
-    MergeSort(a,0,n-1);
+    solve(map);
     for(int i=0;i<n;i++){
-        if(BinSearch(a,0,n-1,a[i]+c)!=-1)cnt++;
+        for(int j=0;j<m;j++){
+            cout<<map[i][j];
+        }cout<<endl;
     }
-    cout<<cnt<<endl;
     return 0;
 }
