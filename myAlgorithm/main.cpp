de4728cfbbc452fbcde1c835899bf947b32567ff
Unknown816 <c19901008@qq.com>
2020-11-16 21:52:09 +0800

    实验8 算法设计与分析课程

--- myAlgorithm/main.cpp
+++ myAlgorithm/main.cpp
@@ -1,94 +1,25 @@
 #include <iostream>
-#include <queue>
+#include <cstring>
+#include <algorithm>
 using namespace std;
-#define MAXN 20
-#define INF 0x3f3f3f3f
-int n=5,W=10;
-int w[]={0,5,2,6,4,3};
-int bestx[MAXN];   //最优解
-int total=1;
-int maxw=-9999;
-int minc=6;
-struct NodeType{
-    int no;   //结点编号
-    int i;   //当前结点在搜索空间中的层次
-    int w;
-    int x[MAXN];
-
-    double ub;   //上界
-    bool operator<(const NodeType &s) const{   //重载<关系函数
-        return ub<s.ub;   //ub越大越优先出局
-    }
-};
-void bound(NodeType &e){   //计算结点上界
-    int i=e.i+1;
-    int sumw=e.w;
-    while((sumw+w[i]<=W)&&i<=n){
-        sumw+=w[i];
-        i++;
-    }
-    e.ub=sumw;
-}
-void EnQueue(NodeType e,priority_queue<NodeType> &qu){
-    if(e.i==n){
-        int cnt=0;
-        for(int j=1;j<n;j++){
-            if(e.x[j])cnt++;
-        }
-        if(e.w>maxw&&cnt<minc){
-            maxw=e.w;
-            for(int j=1;j<n;j++){
-                bestx[j]=e.x[j];
-            }
-        }
-    }else{
-        qu.push(e);
-    }
-}
-void bfs(){
-    int j;
-    NodeType e{},e1{},e2{};
-    priority_queue<NodeType> qu;
-    e.i=0;
-    e.w=0;
-    e.no=total++;
-    for(j=1;j<=n;j++){
-        e.x[j]=0;
-    }
-    bound(e);
-    qu.push(e);   //根结点进队
-    while(!qu.empty()){
-        e=qu.top();
-        qu.pop();
-        if(e.w+w[e.i+1]<=W){   //检查左孩子结点
-            e1.no=total++;
-            e1.i=e.i+1;
-            e1.w=e.w+w[e1.i];
-            for(j=1;j<=n;j++){
-                e1.x[j]=e.x[j];
-            }
-            e1.x[e1.i]=1;
-            bound(e1);
-            EnQueue(e1,qu);   //左孩子进队
-        }
-        e2.no=total++;
-        e2.i=e.i+1;
-        e2.w=e.w;
-        for(j=1;j<=n;j++){
-            e2.x[j]=e.x[j];
-        }
-        e2.x[e2.i]=0;
-        bound(e2);
-        if(e2.ub>=maxw)
-            //cout<<e2.ub<<endl;
-            EnQueue(e2,qu);   //右孩子进队
-    }
-}
-
+#define MAXN 205
+int a[MAXN];//={1,3,8,5,11};
+int b[MAXN];//={0,0,0,0};
 int main() {
-    bfs();
-    for(int i=1;i<=n;i++){
-        cout<<bestx[i]<<" ";
-    }cout<<endl;
+    int n,m;//5,3
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(int i=0;i<n-1;i++){
+        b[i]=a[i+1]-a[i];
+    }
+    sort(b,b+n-1);
+    int sum=a[n-1]-a[0]+1,j=n-2;
+    for(int i=0;i<m-1;i++){
+        sum-=b[j--]-1;
+    }
+    cout<<sum<<endl;
     return 0;
 }
