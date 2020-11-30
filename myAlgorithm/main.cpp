ec8983f2fa8d8fcb9a37684e11a0cccf8010ddab
Unknown816 <c19901008@qq.com>
2020-11-30 15:22:56 +0800

    2020/11/23 算法设计与分析课程

--- myAlgorithm/main.cpp
+++ myAlgorithm/main.cpp
@@ -2,24 +2,47 @@
 #include <cstring>
 #include <algorithm>
 using namespace std;
-#define MAXN 205
-int a[MAXN];//={1,3,8,5,11};
-int b[MAXN];//={0,0,0,0};
+const int MAXN=10005;
+
+struct Stick{
+    int l;
+    int w;
+}s[MAXN];
+bool cmp(Stick x1,Stick x2){
+    if(x1.l==x2.l){
+        return x1.w<x2.w;
+    }else return x1.l<x2.l;
+}
+
 int main() {
-    int n,m;//5,3
-    cin>>n>>m;
-    for(int i=0;i<n;i++){
-        cin>>a[i];
+    int n,t;
+    cin>>t;
+    int len[MAXN];
+    while(t--){
+        memset(len,0,sizeof(len));
+        cin>>n;
+        for(int i=1;i<=n;i++){
+            cin>>s[i].l>>s[i].w;
+        }
+        sort(s+1,s+n+1,cmp);
+        //转化为求包含几个最长上升子序列
+        int cnt=0,ans=0;
+        while(cnt<n){
+            ans++;
+            int maxl=0;
+            for(int i=1;i<=n;i++){
+                if(s[i].w==0)continue;
+                if(len[maxl]<=s[i].w){
+                    len[++maxl]=s[i].w;
+                    cnt++;
+                    s[i].w=0;
+                }else{
+                    int flag=lower_bound(len,len+1+n,s[i].w)-len;
+                    len[flag]=s[i].w;
+                }
+            }
+        }
+        cout<<ans<<endl;
     }
-    sort(a,a+n);
-    for(int i=0;i<n-1;i++){
-        b[i]=a[i+1]-a[i];
-    }
-    sort(b,b+n-1);
-    int sum=a[n-1]-a[0]+1,j=n-2;
-    for(int i=0;i<m-1;i++){
-        sum-=b[j--]-1;
-    }
-    cout<<sum<<endl;
     return 0;
 }
