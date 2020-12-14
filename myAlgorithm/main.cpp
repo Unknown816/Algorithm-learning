57188e0a3a5ac80050db7ef7e573387b1a7cf2a5
Unknown816 <c19901008@qq.com>
2020-12-14 15:26:02 +0800

    2020/12/7提交 算法设计与分析课程

--- myAlgorithm/main.cpp
+++ myAlgorithm/main.cpp
@@ -1,41 +1,53 @@
-#include <iostream>
-#include <algorithm>
+#include<iostream>
 using namespace std;
-const int N = 100;
-double D[N];//={1,2,5,3,6,1};//6段，6个点
 
-void preprocess(){
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>D[i];
-    }
-}
+string str;
+int n;
+const int maxn = 100;
+int dp[maxn][maxn];            //dp[i][j]前i位字符串%n等于j的情况数
+int len;
 
-double Distance(int i,int j){
-    double d1=0,d2=0;
-    if(i>j){
-        int t=i;
-        i=j;
-        j=t;
+/*
+9999999999999X
+3
+*/
+int DP() {
+    //边界，第一位
+    if (str[0] == 'X') {
+        for (int j = 0; j <= n - 1; j++)        //枚举余数
+        {
+            for (int i = 0; i <= 9; i++) {
+                if (i % n == j)
+                    dp[1][j]++;
+            }
+        }
+    } else {
+        int x = str[0] - '0';
+        int j = x % n;
+        dp[1][j]++;
     }
-    for(int x=i;x<j;x++)d1+=D[x];
-    for(int x=0;x<i;x++)d2+=D[x];
-    for(int x=j;;x++){
-        if(D[x]==0)break;
-        d2+=D[x];
+    //后面的位数
+    for (int i = 2; i <= len; i++) {
+        for (int j = 0; j <= n - 1; j++) {
+            if (str[i - 1] == 'X') {
+                for (int k = 0; k <= 9; k++) {
+                    //这里要理解状态转移
+                    int newj = (10 * j + k) % n;
+                    dp[i][newj] += dp[i - 1][j];
+                }
+            } else {
+                int newj = (10 * j + str[i - 1] - '0') % n;
+                dp[i][newj] += dp[i - 1][j];
+            }
+        }
     }
-    return min(d1,d2);
+    return dp[len][0];
 }
 
 int main() {
-    preprocess();
-    int t;
-    cin>>t;
-    while(t--){
-        int i,j;
-        cin>>i>>j;
-        cout<<Distance(i,j)<<endl;
-    }
+    cin >> str;
+    len = str.length();
+    cin >> n;
+    cout << DP() << endl;
     return 0;
 }
